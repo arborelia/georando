566c2401d607670c4e4675cbe195713ee09eba32
Elia Robyn Lake <gh@arborelia.net>
2024-05-05 22:14:24 -0400

    ability to generate an apworld?

--- georando/generate.py
+++ georando/generate.py
@@ -1,14 +1,20 @@
-import pprint
+import json
 import random
+from pathlib import Path
+import zipfile
 from georando.maps import COMMUNITY_MAPS, OFFICIAL_MAPS
+from georando.items import NON_MAP_ITEMS
 from georando.locations import make_goals
 
-
 SETTINGS = {
+    # This is used to identify a manual APWorld.
+    "player_name": "arborelia",
     # How many official (country) maps to use. These maps are harder than most community maps.
     "num_official": 5,
     # How many community maps to use.
     "num_community": 15,
+    # How many maps to start with
+    "num_starting_maps": 3,
     # Names of maps or countries you find easier, and which should be earlier in logic, because you're
     # particularly familiar with them. For example, you might want to put the country you live in here.
     "familiar": ["United States"],
@@ -40,6 +46,8 @@ SETTINGS = {
 
 
 def run():
+    player_name = SETTINGS["player_name"]
+    world_name = f"manual_geoguessr_{player_name}"
     map_pool_official = [
         map
         for map in OFFICIAL_MAPS
@@ -56,16 +64,61 @@ def run():
     selected_maps = random.sample(
         map_pool_official, SETTINGS["num_official"]
     ) + random.sample(map_pool_community, SETTINGS["num_community"])
-    goals = make_goals(selected_maps, SETTINGS["familiar"], skill_modifier)
+    locations = make_goals(selected_maps, SETTINGS["familiar"], skill_modifier)
     n_medals = SETTINGS["medals_to_win"]
-    goals.append(
+    locations.append(
         {
             "category": "Victory",
             "name": f"Victory ({n_medals} gold medals)",
             "requires": [f"Gold Medal:{n_medals}"],
         }
     )
-    pprint.pprint(goals)
+    items = NON_MAP_ITEMS + [
+        {
+            "name": map.name,
+            "progression": True,
+            "category": ["Maps"]
+        } for map in selected_maps
+    ]
+    game_data = {
+        "game": "GeoGuessr",
+        "player": "arborelia",
+        "filler_item_name": "Score Boost +200",
+        "starting_items": [
+            {
+                "items": [
+                    "+10 seconds"
+                ]
+            },
+            {
+                "item_categories": [
+                    "Maps"
+                ],
+                "random": SETTINGS["num_starting_maps"]
+            }
+        ]
+    }
+    region_data = {}
+    world_name = f"manual_geoguessr_{player_name}"
+    with zipfile.ZipFile(f"{world_name}.apworld", "w") as the_zip:
+        world_template = Path("world")
+        for path in world_template.rglob("*"):
+            path_str = str(path)
+            assert path_str.startswith("world/")
+            path_in_zip = world_name + path_str.removeprefix("world")
+            the_zip.write(path_str, arcname=path_in_zip)
+
+        with the_zip.open(f"{world_name}/data/game.json", "w") as out:
+            print(json.dumps(game_data, indent=4), file=out)
+
+        with the_zip.open(f"{world_name}/data/items.json", "w") as out:
+            print(json.dumps(items, indent=4), file=out)
+
+        with the_zip.open(f"{world_name}/data/locations.json", "w") as out:
+            print(json.dumps(locations, indent=4), file=out)
+
+        with the_zip.open(f"{world_name}/data/regions.json", "w") as out:
+            print(json.dumps(region_data, indent=4), file=out)
 
 
 if __name__ == "__main__":
